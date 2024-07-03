5bf51bc1419b48b67cd81f6b096909e95cba6c1f
Matthew Fritz <mattf@burbankparanormal.com>
2024-07-02 19:26:17 -0700

    Add nil checks on the wrappedCore property too

--- src/core.go
+++ src/core.go
@@ -26,35 +26,35 @@ func NewCore(core zapcore.Core) *Core {
 }
 
 func (c *Core) Check(entry zapcore.Entry, checkedEntry *zapcore.CheckedEntry) *zapcore.CheckedEntry {
-	if c == nil {
+	if c == nil || c.wrappedCore == nil {
 		return checkedEntry
 	}
 	return c.wrappedCore.Check(entry, checkedEntry)
 }
 
 func (c *Core) Enabled(level zapcore.Level) bool {
-	if c == nil {
+	if c == nil || c.wrappedCore == nil {
 		return false
 	}
 	return c.wrappedCore.Enabled(level)
 }
 
 func (c *Core) Sync() error {
-	if c == nil {
+	if c == nil || c.wrappedCore == nil {
 		return nil
 	}
 	return c.wrappedCore.Sync()
 }
 
 func (c *Core) With(fields []zapcore.Field) zapcore.Core {
-	if c == nil {
+	if c == nil || c.wrappedCore == nil {
 		return nil
 	}
 	return c.wrappedCore.With(fields)
 }
 
 func (c *Core) Write(entry zapcore.Entry, fields []zapcore.Field) error {
-	if c == nil {
+	if c == nil || c.wrappedCore == nil {
 		return nil
 	}
 	// write to the slice and then return nil since this should always succeed
